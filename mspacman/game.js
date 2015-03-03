6a1c9f068387411c654b907a414a4aabc24aa833
Will Dolan <willdolan@resnet159-148.medford.tufts.edu>
2015-03-03 15:48:32 -0500

    first edits to game.js, broken

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init(){
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');
+
+	//draw board
+	ctx.drawImage(pacman10-hp-sprite.png, 322px, 2px, 463px, 136px);
+
+	//draw ms pac man
+	ctx.drawImage(pacman10-hp-sprite.png, 82px, 22px, 18px, 18px);
+
+}
