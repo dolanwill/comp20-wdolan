89fa4da6bf030c1dad87584f473c375617f9b532
Will Dolan <willdolan@resnet159-148.medford.tufts.edu>
2015-03-03 15:58:01 -0500

    tidied up some syntax, added img parts to js. still broken

--- mspacman/game.js
+++ mspacman/game.js
@@ -2,10 +2,12 @@ function init(){
 	canvas = document.getElementById('game_canvas');
 	ctx = canvas.getContext('2d');
 
-	//draw board
-	ctx.drawImage(pacman10-hp-sprite.png, 322px, 2px, 463px, 136px);
+	  var img = new Image();
+	  img.src = 'pacman10-hp-sprite.png';
+		//draw board
+		ctx.drawImage(pacman10-hp-sprite.png, 322, 2, 463, 136);
 
-	//draw ms pac man
-	ctx.drawImage(pacman10-hp-sprite.png, 82px, 22px, 18px, 18px);
+		//draw ms pac man
+		ctx.drawImage(pacman10-hp-sprite.png, 82, 22, 18, 18);
 
-}
+};
