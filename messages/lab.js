3200fee547e2e0878c49dca79bbd5096644c35b4
Will Dolan <willdolan@wr-130-64-185-201.medford.tufts.edu>
2015-03-05 18:25:44 -0500

    first draft of the lab.js file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here...
+function parse {
+	request = new XMLHTTPRequest();
+	request.open("GET", "messages.json", true);
+	request.onreadystatechange=parseData;
+	request.send();
+}
+
+function parseData{
+	if(request.readyState == 4){
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+
+		for(i = 0; i < converted.length; i++){
+			messagesDiv.innerHTML += "<p> " + converted[i][content];
+		}
+	}
+}
