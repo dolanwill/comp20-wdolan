350e280dc16078daee87784bdb7bc5fb23fe788c
Will Dolan <willdolan@wr-130-64-185-107.medford.tufts.edu>
2015-03-10 18:41:34 -0400

    js syntax fixes

--- messages/lab.js
+++ messages/lab.js
@@ -1,17 +1,20 @@
-function parse {
+function parse() {
 	request = new XMLHTTPRequest();
 	request.open("GET", "data.json", true);
 	request.onreadystatechange=parseData;
 	request.send();
 }
 
-function parseData{
+function parseData() {
 	if(request.readyState == 4){
 		messagesDiv = document.getElementById("messages");
 		converted = JSON.parse(request.responseText);
 
 		for(i = 0; i < converted.length; i++){
-			messagesDiv.innerHTML += "<p> " + converted[i][content];
+			messagesDiv.innerHTML += "<p> " + converted[i][content] + "<p>";
 		}
+	}
+	else{
+		console.log("ready state unacceptable.");
 	}
 }
